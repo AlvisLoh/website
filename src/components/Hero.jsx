d39fb40ec9980929c6b4dc7c533ecbe99018b364
scriptwes1337 <hello@wneoh.com>
2025-03-11 16:09:55 +0800

    Progress on home page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <div id="hero">
+      <h1>Coaching, education consulting, and networking</h1>
+      <p>Get advice from a certified coach</p>
+      <button type="submit" idd="btn-book-session">Book a session</button>
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Alvis Loh</li>
+        <li>Home</li>
+        <li>About</li>
+        <li>Blog</li>
+        <li>Testimonials</li>
+        <li>Book a session</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
